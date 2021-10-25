a93e520ef4e1d1cc47f0a69155ad8e98c39f25fa
Vlad Overchenko <anonim.rab@icloud.com>
2021-10-25 22:57:05 +0300

    refix

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
+
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
+
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 
 	})
+
+	fmt.Println("сервер ожидает")
 	http.ListenAndServe(":8080", nil)
 }
