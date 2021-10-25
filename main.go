b046aca50f6bfbe31edb22bf6eedf5dd114653d1
Vlad Overchenko <anonim.rab@icloud.com>
2021-10-25 23:07:51 +0300

    D:/Git/

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 
 func main() {
 
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/", fs)
+	// fs := http.FileServer(http.Dir("static"))
+	// http.Handle("/", fs)
 
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-
+		http.ServeFile(w, r, "static/login.html")
 	})
 
 	fmt.Println("сервер ожидает")
