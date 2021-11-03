fb2342c2071ff5a703431d2d0c1e4a921e3b9b3f
Ruslan Cheshko <ruslan.cheshko@corezoid.com>
2021-11-03 19:04:29 +0200

    just example

--- main.go
+++ main.go
@@ -1,43 +1,26 @@
 package main
 
 import (
-	"fmt"
+    "log"
 	"html/template"
 	"net/http"
-	"os"
 )
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
 
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
+		w.Write([]byte("Hello. Go to <a href='/login'>/login</a>"))
 	})
 
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, _ := template.ParseFiles("static/login.html")
+		log.Println("Method: ", r.Method)
+		
+		tmpl, _ := template.ParseFiles("./static/login.html")
 		hi := "nothing"
 		tmpl.Execute(w, hi)
 	})
 
-	dat, err := os.ReadFile("goChat/goChat/static/login.html")
-	check(err)
-	fmt.Print(string(dat))
-
-	f, err := os.Open("goChat/goChat/static/login.html")
-	check(err)
-
-	b1 := make([]byte, 15)
-	n1, err := f.Read(b1)
-	check(err)
-	fmt.Printf("%d bytes: %s\n", n1, string(b1[:n1]))
-
-	f.Close()
-
+	
+	log.Println("Listen and serve at http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
